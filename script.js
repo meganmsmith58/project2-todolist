ee14717a84112be5ac8a74ba24809c633cbfce7e
MeganSmith <46550038+meganmsmith58@users.noreply.github.com>
2019-02-27 19:04:28 -0700

    created_html_css_js_files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+//let dateArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
+
+let todaysDay = dayArray [new Date().getDay()];
+document.getElementById('day').innerHTML = todaysDay + ', ';
+let todaysMonth = monthArray [new Date().getMonth()];
+document.getElementById('month').innerHTML = todaysMonth;
+//let todaysDate = dateArray [new Date().getDate()];
+document.getElementById('date').innerHTML = [new Date().getDate()] + ', ';
+document.getElementById('year').innerHTML = [new Date().getFullYear()];
